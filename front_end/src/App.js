b70fc858cad559ea70d6b126589b11184651284b
lch3067 <lch3067@naver.com>
2025-10-13 14:11:30 +0900

    [merge] - 외화, JWT(로그인, 회원가입), 계과개설 병합 완료

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
-=======
-import {  Route, Routes, useNavigate, Navigate } from "react-router-dom";
->>>>>>> b83e7af213d3f3085bd1478cee9a28b7a77362ee
 import BankHome from './pages/main';
 import LoginPage from "./pages/login/login";
 import SignUp from "./pages/signup/signup";
@@ -12,8 +8,7 @@ import { useState } from "react";
 import ForeignProductsPage from "./pages/foreign/ForeignProductsPage";
 import ForeignRatePage from "./pages/foreign/ForeignRatePage";
 
-<<<<<<< HEAD
-// 단계별 화면
+// 계좌 개설 단계별 화면
 import Step1Consent from "./pages/account/Step1Consent";
 import Step2IdVerify from "./pages/account/Step2IdVerify";
 import Step3Info from "./pages/account/Step3Info";
@@ -26,8 +21,6 @@ import Step5Done from "./pages/account/Step5Done";
 
 // App 컴포넌트를 BrowserRouter로 감싸주는 Wrapper
 // 이렇게 하면 App 컴포넌트 내에서 useNavigate를 정상적으로 사용할 수 있습니다.
-=======
->>>>>>> b83e7af213d3f3085bd1478cee9a28b7a77362ee
 function AppWrapper() {
   return (
     <App />
@@ -62,31 +55,27 @@ function App() {
         <Route path="/login" element={<LoginPage onLoginSuccess={handleLoginSuccess} />} />
         <Route path="/signup" element={<SignUp />} />
 
-<<<<<<< HEAD
         {/* 계좌 개설: 각 단계 독립 경로 */}
         <Route path="/account/open" element={
           //<ProtectedRoute>
           <Navigate to="/account/open/step1" replace />
           //</ProtectedRoute>
         } />
-
         <Route path="/account/open/step1" element={<Step1Consent />} />
         <Route path="/account/open/step2" element={<Step2IdVerify />} />
         <Route path="/account/open/step3" element={<Step3Info />} />
         <Route path="/account/open/step4" element={<Step4Product />} />
         <Route path="/account/open/step5" element={<Step5Done />} />
 
-
-=======
         {/* ✅ 외환 라우팅 */}
         <Route path="/foreign" element={<Navigate to="/foreign/rate" replace />} />
         <Route path="/foreign/rate" element={<ForeignRatePage />} />
         <Route path="/foreign/products" element={<ForeignProductsPage />} />
->>>>>>> b83e7af213d3f3085bd1478cee9a28b7a77362ee
-      </Routes>
+
+      </Routes >
 
       <Footer />
-    </div>
+    </div >
   );
 }
 
