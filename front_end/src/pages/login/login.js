5f955c81b57a9588c5fc68e335d374b9024a3bc9
jhyunjoo <gehyunjoo7317@gmail.com>
2025-10-27 18:21:53 +0900

    dev clone

--- front_end/src/pages/login/login.js
+++ front_end/src/pages/login/login.js
@@ -7,7 +7,7 @@ import maintxt from '../../resources/img/e-um.png'
 
 const h = React.createElement;
 
-const API_BASE = process.env.REACT_APP_API_URL;
+const API_BASE = process.env?.REACT_APP_API_URL || "http://localhost:8081";
 
 function Input({ label, name, type, value, onChange, placeholder }) {
   return h("div", null, [
