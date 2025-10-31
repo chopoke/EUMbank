debca0640d05fb1fa4ee986c8dd58d514698e951
jimin <wl3235@naver.com>
2025-10-31 17:32:08 +0900

    외화거래내역/ssl 문제 해결

--- src/main/java/com/boot/eumbank/foreign/service/FxOpenService.java
+++ src/main/java/com/boot/eumbank/foreign/service/FxOpenService.java
@@ -117,7 +117,6 @@ public class FxOpenService {
 
         // 6) 개설 이벤트를 FOREIGN_EXCHANGE_TBL에도 기록 (금액 0, OPEN 이벤트)
         ForeignExchange openEvt = ForeignExchange.builder()
-                .fpNo(1)                               // 임시 상품번호
                 .cNo(saved.getCNo())
                 .aNo(saved.getANo())
                 .feId("OPEN-" + System.currentTimeMillis())
