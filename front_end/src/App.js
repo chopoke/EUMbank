e6217b4b31bbafafceca773166a2a26efebf685b
김영래 <timelimit0317@naver.com>
2025-11-07 12:43:00 +0900

    관리자타임리프연동

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -218,6 +218,9 @@ function App() {
         {/* 공과금페이지 */}
         <Route path="/bills" element={<BillsLanding />} />
 
+        {/* 관리자페이지 */}
+        <Route path="/admin" element={<AdminPage/>} />
+
         {/* 마이페이지 진입 */}
         <Route path="/mypage" element={
           <ProtectedRoute>
