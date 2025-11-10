ed8653470f2cb146d6f03cecff3bcfd75739869b
김영래 <timelimit0317@naver.com>
2025-11-10 20:33:34 +0900

    bill api

--- src/main/java/com/boot/eumbank/bill/infra/KepcoOpenApi.java
+++ src/main/java/com/boot/eumbank/bill/infra/KepcoOpenApi.java
@@ -1,6 +1,5 @@
 package com.boot.eumbank.bill.infra;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -8,17 +7,21 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 
 @Component
-@RequiredArgsConstructor
 public class KepcoOpenApi implements KepcoAdapter {
-    private final @Qualifier("kepcoRestTemplate") RestTemplate rt; // @Bean 등록 필요
-    // TODO: application.yml에 kepco.baseUrl, kepco.apiKey 등록
+
+    private final RestTemplate rt;
+
+    public KepcoOpenApi(@Qualifier("kepcoRestTemplate") RestTemplate rt) {
+        this.rt = rt;
+    }
+
     @Override
     public BigDecimal fetchAvgUnitPrice(int year, int month, String areaCd) {
-        // 임시 구현: 실제 API 파라미터에 맞춰 Map 파싱
-        // String url = baseUrl + "?apiKey=...&year=...&month=...&metroCd=" + areaCd + "&...";
-        // Map resp = rest.getForObject(url, Map.class);
+        // TODO: 실제 API 파라미터 구성 후 호출
+        // String url = baseUrl + "?apiKey=...&year=...&month=...&metroCd=" + areaCd;
+        // Map resp = rt.getForObject(url, Map.class);
         // BigDecimal unit = new BigDecimal(resp.get("avgUnit").toString());
         // return unit;
-        return BigDecimal.valueOf(130.0); // 목업값. 나중에 실제 호출로 교체
+        return BigDecimal.valueOf(130.0); // 목업값
     }
 }
